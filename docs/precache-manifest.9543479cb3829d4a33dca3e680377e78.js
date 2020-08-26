4cf33953db4e432599d6d50148b221379129284a
Alejandro Gómez <agomez@ia.com.mx>
2020-08-26 18:55:02 -0500

    Actualizar carpeta build a docs

---
+++ docs/precache-manifest.9543479cb3829d4a33dca3e680377e78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42679f93bd5f2d38bbdf95a04fe9775b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8cf5cf4faf1198e93cc",
+    "url": "/static/css/main.53c85480.chunk.css"
+  },
+  {
+    "revision": "05792a1f53564e0adfa8",
+    "url": "/static/js/2.879a03b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.879a03b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8cf5cf4faf1198e93cc",
+    "url": "/static/js/main.d467eeb1.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
